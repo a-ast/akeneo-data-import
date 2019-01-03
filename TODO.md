9ea94fa6bcb8fb11acf91befb5cfa68bf107ce11
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-01-03 15:11:38 +0100

    Add example provider that shows how to create images.

--- TODO.md
+++ TODO.md
@@ -12,16 +12,14 @@
 ## Akeneo Import
 
 1. Implement own simple command bus.
-1. ? Move all messenger-related classes to Messenger endpoint.
-1. Test reject/requeue in the MessageHandler after implementing Product models
 1. Create bundle?
+    1. With the consume command that shows proper progress and statuses (requeue etc)
+1. Important: journal/log/report for failed requests in consumer (in order to monitor what happened in MQ).
 
 
 ## Import Commands
 
 1. Implement limitations of type in a command list and overall limit
-1. Implement associations
-1. Implement groups
 1. Test with product models
 1. Implement uploading images
 
---
+++ src/PoC/ProductWithMediaProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\PoC;
+
+use Aa\AkeneoImport\ImportCommand\CommandInterface;
+use Aa\AkeneoImport\ImportCommand\CommandProviderInterface;
+use Aa\AkeneoImport\ImportCommand\Product\UpdateOrCreateProduct;
+use Traversable;
+
+class ProductWithMediaProvider implements CommandProviderInterface
+{
+    /**
+     * @return Traversable|CommandInterface[]
+     */
+    public function getCommands(): Traversable
+    {
+        $mediaFolder = realpath(__DIR__) . '/media/';
+
+        $product = new UpdateOrCreateProduct('test-product-with-media');
+        $product
+            //->setFamily('clothing')
+            ->addImageValue('variation_image', $mediaFolder . 'tshirt.png');
+
+        yield $product;
+    }
+}
